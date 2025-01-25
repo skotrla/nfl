ef225eb7d94d1e8a77fe8d9962b9b196543a3c5a
skotrla <50790449+skotrla@users.noreply.github.com>
2025-01-25 14:51:38 -0600

    Update nflgui.py

--- nflgui.py
+++ nflgui.py
@@ -231,7 +231,7 @@ if db[0]=='an':
         st.dataframe(fdf, use_container_width=True,hide_index=True)
         st.markdown(f'<i>{len(fdf)} rows out of {len(bga)} total rows<br>Last updated: {lastdate}</i>',unsafe_allow_html=True)
 if db[0]=='bga':
-        #connection = sqlite3.connect('c://users//2019//desktop//print//bga.db')
+#       connection = sqlite3.connect('c://users//2019//desktop//print//bga.db')
         flist = [x for x in os.listdir('.') if x.find('bga.db') >= 0]
         if len(flist) == 0:
             flist = [x for x in os.listdir('.') if x.find('bgadb') >= 0]
