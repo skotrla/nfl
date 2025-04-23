e72cd7b6200423e43aba7e3ff33c462301e092d5
skotrla <50790449+skotrla@users.noreply.github.com>
2025-04-22 19:44:38 -0500

    Update nflgui.py

--- nflgui.py
+++ nflgui.py
@@ -309,3 +309,40 @@ if db[0]=='bga':
         #    hide_index=True)
         st.dataframe(fdf, use_container_width=True,hide_index=True)
         st.markdown(f'<i>{len(fdf)} rows out of {len(bga)} total rows<br>Last updated: {lastdate}</i>',unsafe_allow_html=True)
+if db[0]=='anall':
+#        connection = sqlite3.connect('c://users//2019//desktop//print//bga.db')
+#        os.remove('bga.db')
+        flist = [x for x in os.listdir('.') if x.find('bga.db') >= 0]
+        if len(flist) == 0:
+            flist = [x for x in os.listdir('.') if x.find('bgadb') >= 0]
+            flist.sort()
+#            os.system('cat ' + ' '.join(flist) + ' > bga.db')
+            join_files(flist, 'bga.db')
+        connection = sqlite3.connect('bga.db')        
+        connection2 = sqlite3.connect('bga2.db')    
+        bga = pd.read_sql(f'SELECT * FROM arknovap', connection).drop(columns=['index'])
+        bgab = pd.read_sql(f'SELECT * FROM arknovap', connection2).drop(columns=['index'])
+        bga = pd.concat([bga,bgab])
+#        bga['Date'] = pd.to_datetime(bga['Date']).dt.strftime('%Y-%m-%d')
+        bga['Date'] = pd.to_datetime(bga['Date'],format='mixed')
+        bga = bga.sort_values(['Date'],ascending=False)
+        bga['score']=bga['score'].astype('float')
+        winner = bga.groupby(['table']).agg({'score':'max'}).reset_index().rename(columns={'score':'max'})
+        bga = bga.merge(winner,how='inner',on='table')
+        bga['winner']=np.where(bga['score']==bga['max'],True,False)
+        bga = bga.drop(columns='max')    
+#        lastdate = bga['Date'].max()
+        lastdate = pd.read_sql(f'SELECT MAX(Date) as Date FROM lastdate',connection2)['Date'].tolist()[0]
+        connection.close()
+        connection2.close()
+        coll = bga.columns
+        fdf = filter_dataframe(bga,['player'])
+        st.title('Ark Nova Stats '+ str(v))
+        #st.data_editor(
+        #    fdf,
+        #    column_config={
+        #   "Week": st.column_config.NumberColumn(format="%d"),
+        #    "Year": st.column_config.NumberColumn(format="%d"),
+        #    hide_index=True)
+        st.dataframe(fdf, use_container_width=True,hide_index=True)
+        st.markdown(f'<i>{len(fdf)} rows out of {len(bga)} total rows<br>Last updated: {lastdate}</i>',unsafe_allow_html=True)
