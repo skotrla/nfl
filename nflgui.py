191a61371137471cc2f38ca3bf34319b3977cf84
skotrla <50790449+skotrla@users.noreply.github.com>
2025-09-27 15:23:09 -0500

    Update nflgui.py

--- nflgui.py
+++ nflgui.py
@@ -190,6 +190,10 @@ if db[0]=='':
         nfl['Winner'] = np.where(nfl['HActual'] == nfl['RActual'],'Tie',nfl['Winner'])
         nfl['Loser'] = np.where(nfl['HActual'] < nfl['RActual'],nfl['HTeamN'],nfl['RTeamN'])
         nfl['Loser'] = np.where(nfl['HActual'] == nfl['RActual'],'Tie',nfl['Loser'])
+        nfl['SWin'] = np.where(nfl['BWinner'] == nfl['HTeamN'], nfl['HActual'] + nfl['BPoints'] - nfl['RActual'],nfl['RActual'] + nfl['BPoints'] - nfl['HActual'])
+        nfl['SWin'] = np.where(nfl['SWin'] > 0, 1, nfl['SWin'])
+        nfl['SWin'] = np.where(nfl['SWin'] == 0, 0.5, nfl['SWin'])
+        nfl['SWin'] = np.where(nfl['SWin'] < 0, 0, nfl['SWin'])    
         fdf = filter_dataframe(nfl,[])
         st.title('NFL Game Most Recent Model/Spread')
         tactual = fdf['TActual'].min()
@@ -377,6 +381,7 @@ if db[0]=='andb':
         #    hide_index=True)
         st.dataframe(fdf, use_container_width=True,hide_index=True)
         st.markdown(f'<i>{len(fdf)} rows out of {len(bga)} total rows<br>Last updated: {lastdate}</i>',unsafe_allow_html=True)
+
 
 
 
