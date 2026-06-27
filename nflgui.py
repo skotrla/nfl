e398409cc66cbe9756d687cef18875eba5e3d343
skotrla <50790449+skotrla@users.noreply.github.com>
2026-06-27 15:50:44 -0500

    Update nflgui.py

--- nflgui.py
+++ nflgui.py
@@ -361,7 +361,7 @@ if db[0]=='bga':
         bga.set_index('table', inplace=True)
         bga['date_delta'].interpolate(method='index', limit_direction='both', inplace=True)
         bga.reset_index(inplace=True)
-        bga['Date'] = np.where(bga['Date'].isnull(), pd.to_datetime((bga['Date'].min() + pd.TimedeltaIndex(bga['date_delta'], unit='D')).astype('str').str[:10]),bga['Date'])
+        bga['Date'] = np.where(bga['Date'].isnull(), pd.to_datetime((bga['Date'].min() + pd.TimedeltaIndex(pd.to_timedelta(bga['date_delta'], unit='D'))).astype('str').str[:10]),bga['Date'])
         bga = bga.drop(columns=['date_delta'])
 #        dates = pd.read_sql(f'SELECT * FROM arknova', connection).drop(columns=['index'])
 #        datesb = pd.read_sql(f'SELECT * FROM arknova', connection2).drop(columns=['index'])
