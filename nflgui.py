27aef3faf33c7ad322421e83dd5f43cd509c8e0f
skotrla <50790449+skotrla@users.noreply.github.com>
2026-06-27 16:25:13 -0500

    Update nflgui.py

--- nflgui.py
+++ nflgui.py
@@ -385,6 +385,7 @@ if db[0]=='bga':
         #    hide_index=True)
         st.dataframe(fdf, use_container_width=True,hide_index=True)
         st.markdown(f'<i>{len(fdf)} rows out of {len(bga)} total rows<br>Last updated: {lastdate}</i>',unsafe_allow_html=True)
+        st.line_chart(data=fdf, *, x='Date', y='elo', width="stretch", height="content", use_container_width=None)
 if db[0]=='andb':
     if validate():
         connection = sqlite3.connect('bga.db')        
